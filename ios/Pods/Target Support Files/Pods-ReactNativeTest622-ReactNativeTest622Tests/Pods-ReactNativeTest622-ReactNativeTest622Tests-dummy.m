501d1d99e876f6f558296531dd143e39869fe480
Darii Nurgaleev <Darleev@github.com>
2020-10-06 02:03:58 +0700

    Added new application

--- ios/Pods/Target Support Files/Pods-ReactNativeTest622-ReactNativeTest622Tests/Pods-ReactNativeTest622-ReactNativeTest622Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactNativeTest622_ReactNativeTest622Tests : NSObject
-@end
-@implementation PodsDummy_Pods_ReactNativeTest622_ReactNativeTest622Tests
-@end
